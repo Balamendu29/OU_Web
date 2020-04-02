860a8d84fcc4b3f721f2512a927858e82e28049e
Balamendu29 <55770656+Balamendu29@users.noreply.github.com>
2020-04-02 19:56:46 -0400

    Updated the Comment

--- BookStore/BookStore/Models/Book.cs
+++ BookStore/BookStore/Models/Book.cs
@@ -11,11 +11,14 @@ namespace BookStore.Models
         [Key]
         public int Id { get; set; }
 
-        [Required(ErrorMessage = "Please enter Book title.")]
-        [StringLength(20, MinimumLength = 3)]
+        [Required(ErrorMessage = "Please enter Book title...")]
+        [StringLength(50, MinimumLength = 3)]
         public string title { get; set; }
 
+        [Required(ErrorMessage = "Please enter Book Author...")]
+        [StringLength(50, MinimumLength = 3)]
         public string author { get; set; }
+
         public int writtenyear { get; set; }
         public string edition { get; set; }
         public float price { get; set; }
