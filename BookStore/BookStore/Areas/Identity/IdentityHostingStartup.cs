3d47e009e200202008adaf1080bab281866528e0
Balamendu29 <55770656+Balamendu29@users.noreply.github.com>
2020-04-02 16:57:53 -0400

    Login Created

---
+++ BookStore/BookStore/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using BookStore.Areas.Identity.Data;
+using BookStore.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(BookStore.Areas.Identity.IdentityHostingStartup))]
+namespace BookStore.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+                services.AddDbContext<UserAuthDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("UserAuthDBContextConnection")));
+
+                services.AddDefaultIdentity<ApplicationUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireLowercase = false;
+                    options.Password.RequireUppercase = false;
+
+                })
+                    .AddEntityFrameworkStores<UserAuthDBContext>();
+            });
+        }
+    }
+}
---
+++ BookStore/BookStore/Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookStore.Models
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Please enter Book title.")]
+        [StringLength(20, MinimumLength = 3)]
+        public string title { get; set; }
+
+        public string author { get; set; }
+        public int writtenyear { get; set; }
+        public string edition { get; set; }
+        public float price { get; set; }
+    }
+}
